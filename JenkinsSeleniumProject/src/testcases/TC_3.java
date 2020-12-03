c6325bfaaeed1de59e5ade5c55f87338162febb2
Rahul <rahul@host.docker.internal>
2020-12-03 18:27:19 +0530

    Commit all code

---
+++ JenkinsSeleniumProject/src/testcases/TC_3.java
@@ -0,0 +1,17 @@
+package testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_3 {
+	
+	@Test
+	public void testcase1()
+	{
+		System.setProperty("webdriver.chrome.driver", "D://EclipseSelenium//ESelenium//PracticeSelenium//chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://google.com");
+		driver.findElementByName("q").sendKeys("Syllabus");
+		driver.quit();
+	}
+}
